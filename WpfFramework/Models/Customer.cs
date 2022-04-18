fa94994e3ba5d66c7f28b16fb8a991cd75b5a935
Connor Park <kaki104@hanmail.net>
2022-04-18 17:21:38 +0900

    ObservableValidator 이용해 유효성 검사하기

--- WpfFramework/Models/Customer.cs
+++ WpfFramework/Models/Customer.cs
@@ -1,35 +1,53 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace WpfFramework.Models
 {
+    /// <summary>
+    /// Customer
+    /// </summary>
     public partial class Customer : ObservableValidator
     {
         [ObservableProperty]
+        [Required]
+        [MaxLength(5)]
         private string _customerID;
         [ObservableProperty]
+        [Required]
+        [MaxLength(40)]
         private string _companyName;
         [ObservableProperty]
+        [MaxLength(30)]
         private string _contactName;
         [ObservableProperty]
+        [MaxLength(30)]
         private string _contactTitle;
         [ObservableProperty]
+        [MaxLength(60)]
         private string _address;
         [ObservableProperty]
+        [MaxLength(15)]
         private string _city;
         [ObservableProperty]
+        [MaxLength(15)]
         private string _region;
         [ObservableProperty]
+        [MaxLength(10)]
         private string _postalCode;
         [ObservableProperty]
+        [MaxLength(15)]
         private string _country;
         [ObservableProperty]
+        [MaxLength(24)]
+        [Phone]
         private string _phone;
         [ObservableProperty]
+        [MaxLength(24)]
+        [Phone]
         private string _fax;
+        [ObservableProperty]
+        [MaxLength(24)]
+        [EmailAddress]
+        private string _email;
     }
 }
