14a1645328f5236f4619ef3613684f46b8196b31
Connor Park <kaki104@hanmail.net>
2022-04-18 17:21:38 +0900

    ObservableValidator 이용해 유효성 검사하기

--- WpfFramework/ViewModels/CustomerViewModel.cs
+++ WpfFramework/ViewModels/CustomerViewModel.cs
@@ -2,9 +2,12 @@
 using Microsoft.Toolkit.Mvvm.Input;
 using Microsoft.Toolkit.Mvvm.Messaging;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
+using System.Linq;
+using System.Windows;
 using System.Windows.Input;
 using WpfFramework.Bases;
 using WpfFramework.Models;
@@ -19,7 +22,13 @@ namespace WpfFramework.ViewModels
         [ObservableProperty]
         private IList<Customer> _customers;
 
-        public ICommand BackCommand { get; set; }
+        [ObservableProperty]
+        private Customer _selectedCustomer;
+
+        [ObservableProperty] 
+        private string _errorMessage;
+
+        public IRelayCommand SaveCommand { get; set; }
 
         public CustomerViewModel(IDatabaseService databaseService)
         {
@@ -30,10 +39,79 @@ namespace WpfFramework.ViewModels
         private void Init()
         {
             Title = "Customer";
-            BackCommand = new RelayCommand(OnBack);
+
+            SaveCommand = new RelayCommand(Save, 
+                            () => Customers != null 
+                            && Customers.Any(c => string.IsNullOrWhiteSpace(c?.CustomerID)));
+
+            PropertyChanging += CustomerViewModel_PropertyChanging;
+            PropertyChanged += CustomerViewModel_PropertyChanged;
         }
 
-        private void OnBack()
+        private void CustomerViewModel_PropertyChanging(object sender, System.ComponentModel.PropertyChangingEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(SelectedCustomer):
+                    if(SelectedCustomer != null)
+                    {
+                        SelectedCustomer.ErrorsChanged -= SelectedCustomer_ErrorsChanged;
+                        ErrorMessage = string.Empty;
+                    }
+                    break;
+            }
+        }
+
+        private void CustomerViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            switch(e.PropertyName)
+            {
+                case nameof(SelectedCustomer):
+                    if (SelectedCustomer != null)
+                    {
+                        SelectedCustomer.ErrorsChanged += SelectedCustomer_ErrorsChanged;
+                        SetErrorMessage(SelectedCustomer);
+                    }
+                    break;
+            }
+        }
+
+        private void SelectedCustomer_ErrorsChanged(object sender, System.ComponentModel.DataErrorsChangedEventArgs e)
+        {
+            SetErrorMessage(sender as Customer);
+        }
+
+        private void SetErrorMessage(Customer customer)
+        {
+            if(customer == null)
+            {
+                return;
+            }
+            var errors = customer.GetErrors();
+            ErrorMessage = string.Join("\n", errors.Select(e => e.ErrorMessage));
+        }
+
+        /// <summary>
+        /// AddCommand
+        /// </summary>
+        [ICommand]
+        private void Add()
+        {
+            var newCustomer = new Customer();
+            Customers.Insert(0, newCustomer);
+            SelectedCustomer = newCustomer;
+            SaveCommand.NotifyCanExecuteChanged();
+        }
+        private void Save()
+        {
+            MessageBox.Show("Save");
+            SaveCommand.NotifyCanExecuteChanged();
+        }
+        /// <summary>
+        /// BackCommand
+        /// </summary>
+        [ICommand]
+        private void Back()
         {
             WeakReferenceMessenger.Default.Send(new NavigationMessage("GoBack"));
         }
@@ -43,7 +121,7 @@ namespace WpfFramework.ViewModels
             Message = "Navigated";
 
             var datas = await _dbService.GetDatasAsync<Customer>("Select * from [Customers]");
-            Customers = datas;
+            Customers = new ObservableCollection<Customer>(datas);
         }
     }
 }
